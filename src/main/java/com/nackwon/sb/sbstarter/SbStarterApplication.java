8e3be8f39d6a07c3394218aa862656f58b193f12
Nackwon.Do <ahwk321@gmail.com>
2022-08-10 10:38:59 +0900

    add : Project Start

---
+++ src/main/java/com/nackwon/sb/sbstarter/SbStarterApplication.java
@@ -0,0 +1,13 @@
+package com.nackwon.sb.sbstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nackwon/sb/sbstarter/SbStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nackwon.sb.sbstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
