f23fe662c6f75a32d6c655d24727aecea1431db0
Nackwon.Do <ahwk321@gmail.com>
2022-08-19 16:31:40 +0900

    add : Chapter ５　실습 중

---
+++ src/main/java/com/nackwon/sb/sbstarter/domain/user/Role.java
@@ -0,0 +1,15 @@
+package com.nackwon.sb.sbstarter.domain.user;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+
+    GUEST("ROLE_GUEST", "Guest"),
+    USER("ROLE_USER", "User");
+
+    private final String key;
+    private final String title;
+}
--- src/main/java/com/nackwon/sb/sbstarter/web/IndexController.java
+++ src/main/java/com/nackwon/sb/sbstarter/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.nackwon.sb.sbstarter.web;
 
+import com.nackwon.sb.sbstarter.config.auoth.dto.SessionUser;
 import com.nackwon.sb.sbstarter.domain.posts.PostsService;
 import com.nackwon.sb.sbstarter.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
@@ -32,6 +41,7 @@ public class IndexController {
 
         return "posts-update";
     }
+
 
 
 }
